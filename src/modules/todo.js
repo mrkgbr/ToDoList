39a8946f66362f25342a9c711adf873207414187
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-04-19 22:37:27 +0200

    Update functions

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -19,10 +19,10 @@ class Task {
     this.description = description;
     this.date = date;
     this.completed = completed;
-    this.create();
+    this.#create();
   }
 
-  create() {
+  #create() {
     const inbox = document.querySelector(".todo");
 
     const task = document.createElement("div");
@@ -30,32 +30,73 @@ class Task {
     task.setAttribute("id", this.id);
     inbox.appendChild(task);
 
-    const title = document.createElement("p");
-    title.textContent = this.title;
+    const taskForm = document.createElement("form");
+    taskForm.classList.add("task-form");
+    task.appendChild(taskForm);
+
+    const title = document.createElement("input");
+    title.setAttribute("type", "text");
+    title.setAttribute("name", "taskTitle");
+    title.readOnly = true;
+    title.value = this.title;
     title.classList.add("task-title");
-    task.appendChild(title);
+    taskForm.appendChild(title);
 
-    const description = document.createElement("p");
-    description.textContent = this.description;
+    const description = document.createElement("textarea");
+    description.setAttribute("name", "taskDescription");
+    description.readOnly = true;
+    description.value = this.description;
     description.classList.add("task-description");
-    task.appendChild(description);
+    taskForm.appendChild(description);
 
-    const date = document.createElement("p");
-    date.textContent = this.date;
+    const date = document.createElement("input");
+    date.setAttribute("type", "date");
+    date.setAttribute("name", "taskDate");
+    date.readOnly = true;
+    date.value = this.date;
     date.classList.add("task-date");
-    task.appendChild(date);
+    taskForm.appendChild(date);
 
-    const editBtn = document.createElement("button");
-    editBtn.textContent = "EDIT";
+    const editBtn = document.createElement("input");
+    editBtn.setAttribute("type", "submit");
+    editBtn.value = "EDIT";
     editBtn.classList.add("edit-button");
-    task.appendChild(editBtn);
+    taskForm.appendChild(editBtn);
+
+    editBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      editBtn.value = "SAVE";
+      title.readOnly = false;
+      title.classList.toggle("edit");
+      description.readOnly = false;
+      description.classList.toggle("edit");
+      date.readOnly = false;
+      date.classList.toggle("edit");
+      editBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        editBtn.value = "EDIT";
+        title.readOnly = true;
+        title.classList.toggle("edit");
+        description.readOnly = true;
+        description.classList.toggle("edit");
+        date.readOnly = true;
+        date.classList.toggle("edit");
+        const data = parseLocalStorage();
+        data[this.id].task = taskForm.taskTitle.value;
+        data[this.id].description = taskForm.taskDescription.value;
+        data[this.id].date = taskForm.taskDate.value;
+        localStorage.setItem("toDos", JSON.stringify(data));
+        render();
+      });
+    });
 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "DEL";
     deleteBtn.classList.add("delete-button");
-    task.appendChild(deleteBtn);
+    taskForm.appendChild(deleteBtn);
 
-    deleteBtn.addEventListener("click", () => {
+    deleteBtn.addEventListener("click", (del) => {
+      del.preventDefault();
       const data = parseLocalStorage();
       data.splice(this.id, 1);
       document.querySelectorAll(".task-container")[this.id].remove();
